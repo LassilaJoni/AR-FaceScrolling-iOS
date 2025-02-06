09949547fd0fb581cf5b97c60ac33232e0cedf29
LassilaJoni <joni@lassila.live>
2025-02-06 23:37:00 +0200

    Initial Commit

---
+++ AR-FaceScrolling-iOS/AR_FaceScrolling_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_FaceScrolling_iOSApp.swift
+//  AR-FaceScrolling-iOS
+//
+//  Created by Joni Lassila on 6.2.2025.
+//
+
+import SwiftUI
+
+@main
+struct AR_FaceScrolling_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
